71868abd78a003af48ecbfe8c03f8519974774ca
venkata reddy <vasuvenkat9396@gmail.com>
2021-04-11 10:32:22 +0530

    babel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 import { myNumber } from './variable.js';
 
 console.log(myNumber);
+console.log('hai');
+
+class Person{
+    constructor(name,age,gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+let person = new Person('Venkata reddy',22,'Male');
+console.log(person);
